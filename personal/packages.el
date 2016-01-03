49a58bd9d120ca268f376213b2cc747c108b3f2f
Ratson <github@ratson.name>
2016-01-04 00:40:11 +0800

    Add personal layer

---
+++ personal/packages.el
@@ -0,0 +1,11 @@
+(setq personal-packages '(ethan-wspace
+                          w3m))
+
+(defun personal/init-ethan-wspace ()
+  (use-package ethan-wspace
+    :defer t
+    :init
+    (global-ethan-wspace-mode 1)))
+
+(defun personal/init-w3m ()
+  (use-package w3m :defer t))
