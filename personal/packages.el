ceb6fa52dddac90229aca4a6b203a1f30b6183da
Ratson <github@ratson.name>
2016-01-13 01:32:11 +0800

    Add 2 useful org packages

--- personal/packages.el
+++ personal/packages.el
@@ -1,4 +1,6 @@
 (setq personal-packages '(ethan-wspace
+                          org-cliplink
+                          org-journal
                           w3m))
 
 (defun personal/init-ethan-wspace ()
@@ -6,6 +8,12 @@
     :defer t
     :init
     (global-ethan-wspace-mode 1)))
+
+(defun personal/init-org-cliplink ()
+  (use-package org-cliplink :defer t))
+
+(defun personal/init-org-journal ()
+  (use-package org-journal :defer t))
 
 (defun personal/init-w3m ()
   (use-package w3m :defer t))
